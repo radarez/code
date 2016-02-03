871b558b3f06af5d6ecfa8eeec676e9d12ca4335
Adrian Miranda A <ama@radarez.com>
2016-02-03 11:58:24 -0600

    Modulo JS bootstrap

---
+++ JS/modulo_boostrap.js
@@ -0,0 +1,21 @@
+// Namespace for the library
+var MyLibrary = {};
+ 
+// Library definition
+MyLibrary = (function () {
+  // Private variables / properties
+  var p1, p2;
+ 
+  // Private methods
+  function aPrivateMethod() {
+  }
+ 
+  // Public API
+  return {
+    publicMethod: function () {
+    },
+ 
+    anotherPublicMethod () {
+    }
+  }
+}());
